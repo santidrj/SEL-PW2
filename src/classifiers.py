01b0c331a2e1a1f9623ef53a4ab707927172d376
Santiago del Rey <59149491+santidrj@users.noreply.github.com>
2022-05-12 18:24:18 +0200

    Fix creation of splits

--- src/classifiers.py
+++ src/classifiers.py
@@ -1,16 +1,22 @@
 import logging
+from itertools import chain, combinations
 from typing import Tuple
 
 import numpy as np
 from pandas import DataFrame
 
 
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(1, len(s)+1))
+
 class TreeNode:
-    def __init__(self, data, is_leaf = False):
+    def __init__(self, data, is_leaf=False):
         self.left = None
         self.right = None
         self.data = data
-        self.terminal = is_leaf
+        self.is_leaf = is_leaf
 
 
 class CART:
@@ -25,6 +31,10 @@ class CART:
         self.logger = logging.getLogger('CART')
 
         self.validate_input()
+
+    def __str__(self):
+        s = "Preorder representation:\n"
+        return s + self._print_tree(self.root)
 
     def validate_input(self):
         if self.max_depth == 0:
@@ -44,24 +54,25 @@ class CART:
         left = node.data['split'][0].copy()
         right = node.data['split'][1].copy()
         if len(left) < 1 or len(right) < 1:
-            node.terminal = True
+            node.data = left.iloc[0, -1] if len(left) >= 1 else right.iloc[0, -1]
+            node.is_leaf = True
             return
 
         del (node.data['split'])
         if 0 < self.max_depth <= depth:
-            node.left = TreeNode(left, True)
-            node.right = TreeNode(right, True)
+            node.left = TreeNode(left.iloc[0, -1], True)
+            node.right = TreeNode(right.iloc[0, -1], True)
             return
 
         if len(left) <= self.min_size:
-            node.left = TreeNode(left, True)
+            node.left = TreeNode(left.iloc[0, -1], True)
         else:
             left_split = self._get_split(left)
             node.left = TreeNode(left_split)
             self._split(node.left, depth + 1)
 
         if len(right) <= self.min_size:
-            node.right = TreeNode(right, True)
+            node.right = TreeNode(right.iloc[0, -1], True)
         else:
             right_split = self._get_split(right)
             node.right = TreeNode(right_split)
@@ -81,7 +92,7 @@ class CART:
             # exit if it finds a split with the best possible gini index.
             if best_gini == 0.0:
                 break
-        self.logger.info(f'Final split: {best_split} with gini index {best_gini} using feature {best_feature}')
+        self.logger.info(f'Final split with gini index {best_gini} using feature {best_feature} and value/s {best_value}')
         return {'feature': best_feature, 'value': best_value, 'split': best_split}
 
     def _generate_splits(self, X, col):
@@ -91,11 +102,10 @@ class CART:
         if numerical:
             col_values = sorted(col_data.unique())
         else:
-            col_values = col_data.unique()
+            col_values = powerset(col_data.unique())
 
-        for v in col_data:
+        for v in col_values:
             gini_idx, split = self._feature_splits(X, col_data, v, numerical)
-            self.logger.debug(f'Tentative split for column {col}: {split}')
             if gini_idx < best_gini:
                 best_split = split
                 best_gini = gini_idx
@@ -112,7 +122,7 @@ class CART:
         if numerical:
             mask = col_data <= v
         else:
-            mask = col_data == v
+            mask = col_data.isin(v)
         l_split = X[mask]
         r_split = X[~mask]
         split = (l_split, r_split)
@@ -139,3 +149,15 @@ class CART:
                 # weight the group score by size
                 gini += (g.shape[0] / n_samples) * score
         return gini
+
+    def _print_tree(self, node, depth=0):
+        if node.is_leaf:
+            return f'At level {depth} {node.data}\n'
+        else:
+            if node.data['feature'] in self.numerical_columns:
+                s = f'At level {depth} [{node.data["feature"]} <= {node.data["value"]}?]\n'
+            else:
+                s = f'At level {depth} [{node.data["feature"]} is {node.data["value"]}?]\n'
+            s += self._print_tree(node.left, depth + 1)
+            s += self._print_tree(node.right, depth + 1)
+            return s
--- tests.py
+++ tests.py
@@ -6,12 +6,14 @@ from src.classifiers import CART
 
 logging.basicConfig(level=logging.DEBUG)
 
-df = pd.DataFrame({'eye-colour': ['blue', 'blue', 'brown', 'green', 'green', 'brown', 'green', 'blue'],
-                   'hair-colour': ['blonde', 'brown', 'brown', 'brown', 'brown', 'brown', 'blone', 'brown'],
-                   'height': [1.96, 1.75, 1.75, 1.63, 1.96, 1.45, 1.53, 1.69],
-                   'class': ['c+', 'c+', 'c-', 'c-', 'c+', 'c-', 'c-', 'c+']})
+df = pd.DataFrame({'eye-colour': ['blue', 'blue', 'brown', 'green', 'green', 'brown', 'green', 'blue', 'red', 'red'],
+                   'hair-colour': ['blonde', 'brown', 'brown', 'green', 'brown', 'brown', 'blonde', 'brown', 'green',
+                                   'blonde'],
+                   'height': [1.96, 1.75, 1.75, 1.63, 1.96, 1.45, 1.53, 1.69, 1.49, 1.85],
+                   'class': ['c+', 'c+', 'c-', 'c-', 'c+', 'c-', 'c-', 'c+', 'c-', 'c+']})
 
 print(df)
 cart = CART()
 labels = df['class'].to_numpy()
 cart.fit(df)
+print(cart)
